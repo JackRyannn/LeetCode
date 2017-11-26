b4ea861ce8a0a922a4c3db04ea7462b2e4d2b311
JackRyannn <982767079@qq.com>
2017-11-26 22:45:08 +0800

    commit11.26

--- 62.py
+++ 62.py
@@ -1,20 +1,9 @@
-# 走出独一无二的路径的个数，采用组合C，编程简单但是不好想。
 class Solution(object):
-    def uniquePaths(self, m, n):
+    def permute(self, nums):
         """
-        :type m: int
-        :type n: int
-        :rtype: int
+        :type nums: List[int]
+        :rtype: List[List[int]]
         """
-        if n == 1 or m == 1:
-            return 1
-        p = n-1
-        q = m+n-2
-        r = 1
-        for i in range(p+1,q+1):
-            r *= i
-        for i in range(1,q - p + 1):
-            r /= i
-            print(i)
-        return r
-print(Solution().uniquePaths(3,7))
+
+
+print(Solution().permute([1,2,3]))
